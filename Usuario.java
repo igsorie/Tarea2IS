0380c60b7b3d331005a56a39681e0e273183a40e
Ignacio Sosa <62818128+igsorie@users.noreply.github.com>
2020-04-04 20:03:46 -0300

    Se agrega en la clase usuarios los getters, setters y constructor

--- Usuario.java
+++ Usuario.java
@@ -5,5 +5,49 @@ public class Usuario {
     	private int contrasena;
     	private int edad;
 	private cantHijos;
-
+	public Usuario(){
+		this.saldo = 0;
+	}
+	public void setNombre(String unNombre){
+        	nombre = unNombre;
+   	}
+    	public String getNombre(){
+        	return nombre;
+    	}	
+	
+	public void setSaldo(int unSaldo){
+		saldo = unSaldo;
+	}
+	
+	public int getSaldo(){
+		return saldo;
+	}
+	
+	public void setCedula(int unaCedula){
+		cedula = unaCedula;
+	}
+	public int getCedula(){
+		return cedula;
+	}
+	
+	public void setEdad(int unaEdad){
+		edad = unaEdad;
+	}
+	public int getEdad(){
+		return edad;
+	}
+	
+	public void setContrasena(int unaContra){
+		contrasena = unaContra;
+	}
+	public int getContrasena(){
+		return contrasena;
+	}
+	
+	public void setHijos(int unHijo){
+			cantHijos = unHijo;
+	}
+	public int getCantHijos(){
+		return cantHijos;
+	}
 }
